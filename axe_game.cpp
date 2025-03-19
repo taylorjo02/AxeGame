8e02b75b9c0b9946b4e0d414177c8af747eeddf7
taylorjo02 <joshua.d.taylor122@gmail.com>
2025-03-18 23:16:56 -0400

    feat: detect key presses for left and right movement

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,14 +1,18 @@
 #include "raylib.h"
 int main()
 {
-  int width;
-  int height;
-
-  width = 350;
-  height = 200;
+  // window dimensions
+  int width{350};
+  int height{200};
 
   InitWindow(width, height, "Axe Game");
 
+  // circle coordinates
+  int circle_x{175};
+  int circle_y{100};
+  float circle_radius{25.0};
+
+  SetTargetFPS(60);
 
   // WindowShouldClose will return true if X icon or Escape Key are pressed
   while (WindowShouldClose() == false)
@@ -16,8 +20,24 @@ int main()
     // handles the setup of the window to the screen
     BeginDrawing();
     // makes the canvas stay the same color and avoid flickering as new frames are buffered
-    ClearBackground(RED);
+    ClearBackground(WHITE);
+
+    // Game logic beings
+
+    DrawCircle(circle_x, circle_y, circle_radius, BLUE);
     // handles the teardown of the window
+
+    if (IsKeyDown(KEY_D))
+    {
+      circle_x = circle_x + 10;
+    }
+
+    if (IsKeyDown(KEY_A))
+    {
+      circle_x = circle_x - 10;
+    }
+
+    // Game logic ends
     EndDrawing();
   }
 }
