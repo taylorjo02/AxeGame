51a10155eb8575fbecfcd5c35d8cd63c3ae48c65
taylorjo02 <joshua.d.taylor122@gmail.com>
2025-03-19 00:06:21 -0400

    feat: finish collision logic and end game scenario

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,16 +2,44 @@
 int main()
 {
   // window dimensions
-  int width{350};
-  int height{200};
+  int width{800};
+  int height{450};
 
   InitWindow(width, height, "Axe Game");
 
   // circle coordinates
-  int circle_x{175};
-  int circle_y{100};
+  int circle_x{200};
+  int circle_y{200};
   float circle_radius{25.0};
 
+  // circle edges
+
+  float l_circle_x{circle_x - circle_radius};
+  float r_circle_x{circle_x + circle_radius};
+  float u_circle_y{circle_y - circle_radius};
+  float b_circle_y{circle_y + circle_radius};
+
+  // axe coords
+
+  int axe_x{300};
+  int axe_y{0};
+  int axe_width{50};
+  int axe_height{50};
+  int direction{10};
+
+  // axe edges
+
+  int l_axe_x{axe_x};
+  int r_axe_x{axe_x + axe_width};
+  int u_axe_y{axe_y};
+  int b_axe_y{axe_y + axe_height};
+
+  bool collision_with_axe = 
+                          (b_axe_y >= u_circle_y) && 
+                          (u_axe_y <= b_circle_y) && 
+                          (l_axe_x <= r_circle_x) && 
+                          (r_axe_x >= l_circle_x);
+  
   SetTargetFPS(60);
 
   // WindowShouldClose will return true if X icon or Escape Key are pressed
@@ -22,20 +50,57 @@ int main()
     // makes the canvas stay the same color and avoid flickering as new frames are buffered
     ClearBackground(WHITE);
 
-    // Game logic begins
-
-    DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-
-    if (IsKeyDown(KEY_D) && circle_x < 350)
+    if (collision_with_axe)
     {
-      circle_x = circle_x + 10;
+      DrawText("Game Over!", 350, 200, 20, RED);
     }
-
-    if (IsKeyDown(KEY_A) && circle_x > 0)
+    else
     {
-      circle_x = circle_x - 10;
-    }
 
+      // Game logic begins
+
+      // updates the edges
+
+      l_circle_x = circle_x - circle_radius;
+      r_circle_x = circle_x + circle_radius;
+      u_circle_y = circle_y - circle_radius;
+      b_circle_y = circle_y + circle_radius;
+
+      l_axe_x = axe_x;
+      r_axe_x = axe_x + axe_width;
+      u_axe_y = axe_y;
+      b_axe_y = axe_y + axe_height;
+
+      // update collision with axe
+
+      collision_with_axe = 
+                          (b_axe_y >= u_circle_y) && 
+                          (u_axe_y <= b_circle_y) && 
+                          (l_axe_x <= r_circle_x) && 
+                          (r_axe_x >= l_circle_x);
+
+      DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+      DrawRectangle(axe_x, axe_y, axe_width, axe_height, RED);
+
+      // move the axe up and down
+
+      axe_y += direction;
+
+      if (axe_y > height - 50 || axe_y < 0)
+      {
+        direction = -direction;
+      }
+
+      if (IsKeyDown(KEY_D) && circle_x < width - 0)
+      {
+        circle_x += 10;
+      }
+
+      if (IsKeyDown(KEY_A) && circle_x > 0)
+      {
+        circle_x -= 10;
+      }
+    }
     // Game logic ends
     EndDrawing();
   }
